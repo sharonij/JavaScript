0bf81b23c147f45aca85fe2584d42d2f91f80293
Sharon Agwah <47524325+sharonij@users.noreply.github.com>
2022-06-23 12:36:54 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+   //My First Javascript code!
+   alert("Hello,My name is Sharon Agwah. I'm 5'7 tall. I'm Nigerian.")
+   let fullName = 'Sharon Agwah' 
+   let height = "5'7"
+   let country = 'Nigeria'
+   console.log (fullName);
+   console.log (height);
+   console.log (country);
